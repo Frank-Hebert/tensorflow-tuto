1f573deb4147ecc5a1d8e4e61152f9e01a272677
frank-hebert <fhebert89@gmail.com>
2021-01-07 21:20:00 -0500

    Modification du gitignore pour le venv

--- tutorial_1.py
+++ tutorial_1.py
@@ -1,2 +1,17 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"    
 import tensorflow as tf
 
+
+# Iintiatilization of Tensors
+
+x = tf.constant(4.0, shape=(1,1))
+print(x)
+y = tf.eye(3)
+print(y)
+
+# Mathematical Operations
+
+# Indexing
+
+# Reshaping
